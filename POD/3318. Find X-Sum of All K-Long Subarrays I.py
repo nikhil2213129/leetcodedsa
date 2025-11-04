584e629984b18849fb51195c1fa68ab18e5b384c
M. Nikhil Kumar <123869983+nikhil2213129@users.noreply.github.com>
2025-11-04 20:42:33 +0530

    Add findXSum method for k-long subarrays
    Implement a solution to find the X-sum of all k-long subarrays.

---
+++ POD/3318. Find X-Sum of All K-Long Subarrays I.py
@@ -0,0 +1,26 @@
+class Solution:
+    def findXSum(self, nums: List[int], k: int, x: int) -> List[int]:
+        k_ele_maps = {}
+        answer = [0] * (len(nums) - k + 1)
+
+        for i in range(k):
+            if nums[i] not in k_ele_maps:
+                k_ele_maps[nums[i]] = 1
+            else:
+                k_ele_maps[nums[i]] += 1
+
+        for i in range(len(nums) - k + 1):
+            top = sorted(k_ele_maps.items(), key=lambda kv: (kv[1], kv[0]), reverse=True)[:x]
+            answer[i] = sum(v * f for v, f in top)
+            out_num = nums[i]
+            k_ele_maps[out_num] -= 1
+            if k_ele_maps[out_num] == 0:
+                del k_ele_maps[out_num]
+            if i + k < len(nums):
+                in_num = nums[i + k]
+                if in_num not in k_ele_maps:
+                    k_ele_maps[in_num] = 1
+                else:
+                    k_ele_maps[in_num] += 1
+
+        return(answer)  
