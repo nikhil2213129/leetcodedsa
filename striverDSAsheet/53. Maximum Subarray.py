bdb6b974ad4a845fee92830c42a1cf32ac8885a9
M. Nikhil Kumar <123869983+nikhil2213129@users.noreply.github.com>
2026-01-04 10:58:08 +0530

    Create 53. Maximum Subarray.py

---
+++ striverDSAsheet/53. Maximum Subarray.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        ans = 0
+        for i in range(len(nums)):
+            cur_sum = 0
+            for j in range(i,len(nums)):
+                cur_sum += nums[j]
+                ans = max(ans,cur_sum)
+        return ans
+        
