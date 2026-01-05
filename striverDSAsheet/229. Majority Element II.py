88a6d884a2002d94917fe8a7424fdb2162d5bee5
M. Nikhil Kumar <123869983+nikhil2213129@users.noreply.github.com>
2026-01-05 12:11:55 +0530

    Create 229. Majority Element II.py

---
+++ striverDSAsheet/229. Majority Element II.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        cand = []
+        cand1 = cand2 = None
+        count1 = count2 = 0
+
+        for num in nums:
+            if num == cand1:
+                count1 += 1
+            elif num == cand2:
+                count2 += 1
+            elif count1 == 0:
+                cand1 = num
+                count1 = 1
+            elif count2 == 0:
+                cand2 = num
+                count2 = 1
+            else:
+                count1 -= 1
+                count2 -= 1
+        if nums.count(cand1) > len(nums) // 3:
+            cand.append(cand1)
+        if cand2 != cand1 and nums.count(cand2) > len(nums) // 3:
+            cand.append(cand2)
+
+        return cand
