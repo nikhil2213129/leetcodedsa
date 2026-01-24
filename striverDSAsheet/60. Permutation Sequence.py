9e5f5d4cb8251a502a6347be3b6bc04fbe84d79e
M. Nikhil Kumar <123869983+nikhil2213129@users.noreply.github.com>
2026-01-24 10:20:46 +0530

    Add Solution class for generating permutation sequence

---
+++ striverDSAsheet/60. Permutation Sequence.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def getPermutation(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: str
+        """
+        import math
+        numbers = range(1,n+1)
+        permutation = ""
+        k -= 1
+        while n>0:
+            n -= 1
+            index,k = divmod(k,math.factorial(n))
+            permutation += str(numbers[index])
+            numbers.remove(numbers[index])
+        return permutation
+        
