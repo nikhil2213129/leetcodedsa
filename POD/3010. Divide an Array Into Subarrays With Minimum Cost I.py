3b3a0c46b8efcaebe164495a7ab83d51f994c4cc
M. Nikhil Kumar <123869983+nikhil2213129@users.noreply.github.com>
2026-02-01 19:01:04 +0530

    Create 3010. Divide an Array Into Subarrays With Minimum Cost I.py

---
+++ POD/3010. Divide an Array Into Subarrays With Minimum Cost I.py
@@ -0,0 +1,8 @@
+# Added using AI
+class Solution:
+    def minimumCost(self, nums: List[int]) -> int:
+        ans = nums[0]
+        nums[1:] = sorted(nums[1:])
+        ans += nums[1]
+        ans += nums[2]
+        return ans
