679086a7cff828792090f943fcd1e6127bea1053
M. Nikhil Kumar <123869983+nikhil2213129@users.noreply.github.com>
2026-03-21 08:55:35 +0530

    Create 3643. Flip Square Submatrix Vertically.py

---
+++ POD/3643. Flip Square Submatrix Vertically.py
@@ -0,0 +1,8 @@
+#translated using AI
+class Solution:
+    def reverseSubmatrix(self, grid, x, y, k):
+        for i in range(k // 2):
+            for j in range(k):
+                grid[x + i][y + j], grid[x + k - 1 - i][y + j] = \
+                grid[x + k - 1 - i][y + j], grid[x + i][y + j]
+        return grid
